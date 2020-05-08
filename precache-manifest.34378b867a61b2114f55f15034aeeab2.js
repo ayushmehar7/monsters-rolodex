6d75aeb1bdbff8a94c28d9bb8a7b6e41125ac7cb
Ayush Mehar <ayushm19100@iiitnr.edu.in>
2020-05-08 17:36:58 +0530

    Updates

---
+++ precache-manifest.34378b867a61b2114f55f15034aeeab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc9fab6bd92e68d3caa387cecd6958a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e29ad68b9902b871fa30",
+    "url": "/monsters-rolodex/static/css/main.be449386.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29ad68b9902b871fa30",
+    "url": "/monsters-rolodex/static/js/main.288e45f8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
